0dc3a2478f294752b74ad5d19c61ac4706c3e7a5
Erfan Seidipoor <erfan.seidipoor@gmail.com>
2023-03-09 00:47:26 +0330

    socialMedia component

---
+++ libs/website-components/src/lib/SocialMedia/index.stories.tsx
@@ -0,0 +1,20 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { faker } from "@faker-js/faker";
+import { SocialMedia } from ".";
+
+export default {
+  component: SocialMedia,
+  title: "SocialMedia",
+} as ComponentMeta<typeof SocialMedia>;
+
+const Template: ComponentStory<typeof SocialMedia> = (args) => {
+  return <SocialMedia {...args} />;
+};
+
+export const Primary = Template.bind({});
+Primary.args = {
+  email: faker.image.imageUrl(),
+  instagram: faker.image.imageUrl(),
+  telegram: "test",
+  whatsApp: faker.image.imageUrl(),
+};
